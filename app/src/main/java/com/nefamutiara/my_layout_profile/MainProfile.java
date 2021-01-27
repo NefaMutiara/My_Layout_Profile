e0791ee9c0f46fa2b2d8deebd55f8e1a15c013ec
Juan Alvis Yudatama <73069332+juanalvis213@users.noreply.github.com>
2021-01-27 18:37:12 +0700

    commitpunyanefa

---
+++ app/src/main/java/com/nefamutiara/my_layout_profile/MainProfile.java
@@ -0,0 +1,16 @@
+package com.nefamutiara.my_layout_profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nefamutiara.my_layout_profile.R;
+
+public class MainProfile extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_profilku);
+    }
+}
